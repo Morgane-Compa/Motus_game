c5e2b0dfd522063beb2003e97ac0b90d30a91916
Morgane-Compa <morgane.compafloury@gmail.com>
2024-01-15 16:52:35 +0100

    header done ne reste plus qu'à faire la modale pour les règles

--- motus_game/src/app/components/header/header.component.ts
+++ motus_game/src/app/components/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  userPseudo: string = '';  // Variable pour stocker le nom d'utilisateur
+
+  ngOnInit() {
+    // Récupère le pseudo de l'utilisateur depuis le localStorage
+    const storedUserName = localStorage.getItem('userName');
+
+    // Vérifie si le pseudo existe avant de l'assigner à la variable (mesure de sécurité)
+    if (storedUserName) {
+      this.userPseudo = storedUserName;
+    }
+  }
 }
